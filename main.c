fade0cd4a98ea5b0572b66c8de4ee3011526387a
Aniketh109 <anikeths@ualberta.ca>
2026-03-13 15:29:15 -0600

    All main.c libraries

--- main.c
+++ main.c
@@ -1,16 +1,30 @@
+// Include FreeRTOS Libraries
 #include "FreeRTOS.h"
-#include "PmodOLED.c"
-#include "pmodkypd.c"
-#include "OledGrph.c"
-#include "OLEDControllerCustom.h"
-#include <stdlib.h>
+#include "task.h"
+#include "queue.h"
 
+// Include xilinx Libraries
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xscugic.h"
+#include "xil_exception.h"
+#include "xil_printf.h"
+#include "xil_cache.h"
+
+// Other miscellaneous libraries
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "pmodkypd.h"
+#include "sleep.h"
+#include "PmodOLED.h"
+#include "OLEDControllerCustom.h"
 
 #define STATE_SHOW_SEQ 0
 #define STATE_INPUT 1
 #define STATE_GAME_OVER 2
-#define STATE_WIN_ROUND 2
-#define STATE_START 3
+#define STATE_WIN_ROUND 3
+#define STATE_START 4
 
 u8 sequence[20];
 
